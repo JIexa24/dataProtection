a56c40ae19e1c4735e32a5926fa358df06e90892
Alexey R <lexa260011@ngs.ru>
2018-09-10 01:11:23 +0700

    rsa

--- include/chiphersl.h
+++ include/chiphersl.h
@@ -12,8 +12,8 @@ long int vernam_encode(char* in, char* out, char* key);
 long int vernam_decode(char* in, char* out, char* key);
 int test_prime_too_num(long int p, long int e);
 long int generate_prime_too_number(long int e);
-void rsa_generate();
-void rsa_encode(char* in, char* out);
-void rsa_decode(char* in, char* out);
+int rsa_generate();
+long int rsa_encode(char* in, char* out);
+long int rsa_decode(char* in, char* out);
 
 #endif
--- src/chiphersl.c
+++ src/chiphersl.c
@@ -1,25 +1,22 @@
 #include "../include/main.h"
 
 long int vernam_decode(char* in, char* out, char* key) {
-  srand(time(NULL));
-  int fdin =  open(in, O_RDONLY);
-  int fdout = open(out, O_WRONLY | O_CREAT | O_TRUNC, 0666);
-  int fdkey = open(key, O_RDONLY);
-  if (fdin == -1) {
-    printf("Can't open file %s\n", in);
-    return -1;
-  }
-  if (fdout == -1) {
-    printf("Can't open file %s\n", out);
-    return -1;
-  }
-  if (fdkey == -1) {
-    printf("Can't open file %s\n", key);
-    return -1;
-  }
+  int fdin, fdout, fdkey;
   char c = '\0';
   char keych = '\0';
   long int k = 0;
+  if ((fdin =  open(in, O_RDONLY)) == -1) {
+    printf("Can't open file %s\n", in);
+    return -1;
+  }
+  if ((fdout = open(out, O_WRONLY | O_CREAT | O_TRUNC, 0666)) == -1) {
+    printf("Can't open file %s\n", out);
+    return -1;
+  }
+  if ((fdkey = open(key, O_RDONLY)) == -1) {
+    printf("Can't open file %s\n", key);
+    return -1;
+  }
   while (read(fdin, &c, 1) != 0) {
     if (read(fdkey, &keych, 1) == 0) {
       printf("\nlength files is not equal\n");
@@ -36,24 +33,23 @@ long int vernam_decode(char* in, char* out, char* key) {
 }
 
 long int vernam_encode(char* in, char* out, char* key) {
-  long int fdin =  open(in, O_RDONLY);
-  int fdout = open(out, O_WRONLY | O_CREAT | O_TRUNC, 0666);
-  int fdkey = open(key, O_WRONLY | O_CREAT | O_TRUNC, 0666);
-  if (fdin == -1) {
-    printf("Can't open file %s\n", in);
-    return -1;
-  }
-  if (fdout == -1) {
-    printf("Can't open file %s\n", out);
-    return -1;
-  }
-  if (fdkey == -1) {
-    printf("Can't open file %s\n", key);
-    return -1;
-  }
+  int fdin, fdout, fdkey;
   char c = '\0';
   char keych = '\0';
   long int k = 0;
+  srand(time(NULL));
+  if ((fdin =  open(in, O_RDONLY)) == -1) {
+    printf("Can't open file %s\n", in);
+    return -1;
+  }
+  if ((fdout = open(out, O_WRONLY | O_CREAT | O_TRUNC, 0666)) == -1) {
+    printf("Can't open file %s\n", out);
+    return -1;
+  }
+  if ((fdkey = open(key, O_WRONLY | O_CREAT | O_TRUNC, 0666)) == -1) {
+    printf("Can't open file %s\n", key);
+    return -1;
+  }
   while (read(fdin, &c, 1) != 0) {
     keych = (rand() % 256);//ascii table
     c = c ^ keych;
@@ -85,11 +81,21 @@ long int generate_prime_too_number(long int e) {
     return p;
 }
 
-void rsa_generate() {
+int rsa_generate() {
   long int p, q, e;
   long int eiler_res;
   long int d, n;
   int fdpub, fdpriv;
+
+  if ((fdpub = open("~/.keyrsa.pub",  O_WRONLY | O_CREAT | O_TRUNC, 0666)) == -1) {
+    printf("Can't open file .keyrsa.pub\n");
+    return -1;
+  }
+  if ((fdpriv = open("~/.keyrsa",  O_WRONLY | O_CREAT | O_TRUNC, 0600)) == -1) {
+    printf("Can't open file .keyrsa\n");
+    return -1;
+  }
+
   do {
     p = generate_prime_number();
     q = generate_prime_number();
@@ -101,16 +107,7 @@ void rsa_generate() {
   e = generate_prime_too_number(eiler_res);
 
   d = fast_pow(e, (unsigned long int)-1, eiler_res);
-  fdpub = open("~/.keyrsa.pub",  O_WRONLY | O_CREAT | O_TRUNC, 0666);
-  if (fdpub == -1) {
-    printf("Can't open file .keyrsa.pub\n");
-    return;
-  }
-  fdpriv = open("~/.keyrsa",  O_WRONLY | O_CREAT | O_TRUNC, 0600);
-  if (fdpriv == -1) {
-    printf("Can't open file .keyrsa\n");
-    return;
-  }
+
   write(fdpub, &e, 8);
   write(fdpub, &n, 8);
   write(fdpriv, &d, 8);
@@ -119,14 +116,73 @@ void rsa_generate() {
   close(fdpriv);
   //pubkey - e, n
   //prkey - d,n
+  return 0;
 }
 
-void rsa_encode(char* in, char* out) {
+long int rsa_encode(char* in, char* out) {
   int fdin, fdout, fdkey;
+  int pubkey_e, pubkey_n;
+  long int k = 0;
+  char c = '\0';
+  if ((fdin =  open(in, O_RDONLY)) == -1) {
+    printf("Can't open file %s\n", in);
+    return -1;
+  }
+  if ((fdout = open(out, O_WRONLY | O_CREAT | O_TRUNC, 0666)) == -1) {
+    printf("Can't open file %s\n", out);
+    return -1;
+  }
+  if ((fdkey = open("~/.keyrsa.pub", O_RDONLY)) == -1) {
+    printf("Can't open file %s\n", "~/.keyrsa.pub");
+    return -1;
+  }
 
+  if (read(fdkey, &pubkey_e, 1) == 0) {
+    printf("Key e loss %s\n", "~/.keyrsa.pub");
+  }
+
+  if (read(fdkey, &pubkey_n, 1) == 0) {
+    printf("Key n loss %s\n", "~/.keyrsa.pub");
+  }
+
+  while (read(fdin, &c, 1) != 0) {
+    c = fast_pow(c, pubkey_e, pubkey_n);
+    write(fdout, &c, 1);
+    ++k;
+  }
+  return k;
 }
 
-void rsa_decode(char* in, char* out) {
+long int rsa_decode(char* in, char* out) {
   int fdin, fdout, fdkey;
+  int privkey_d, privkey_n;
+  long int k = 0;
+  char c = '\0';
+  if ((fdin =  open(in, O_RDONLY)) == -1) {
+    printf("Can't open file %s\n", in);
+    return -1;
+  }
+  if ((fdout = open(out, O_WRONLY | O_CREAT | O_TRUNC, 0666)) == -1) {
+    printf("Can't open file %s\n", out);
+    return -1;
+  }
+  if ((fdkey = open("~/.keyrsa", O_RDONLY)) == -1) {
+    printf("Can't open file %s\n", "~/.keyrsa");
+    return -1;
+  }
 
+  if (read(fdkey, &privkey_d, 1) == 0) {
+    printf("Key d loss %s\n", "~/.keyrsa");
+  }
+
+  if (read(fdkey, &privkey_n, 1) == 0) {
+    printf("Key n loss %s\n", "~/.keyrsa");
+  }
+
+  while (read(fdin, &c, 1) != 0) {
+    c = fast_pow(c, privkey_d, privkey_n);
+    write(fdout, &c, 1);
+    ++k;
+  }
+  return k;
 }
