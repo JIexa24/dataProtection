3ecc453279a0ff71dd1998925508f29ac01d313c
Alexey R <lexa260011@ngs.ru>
2018-10-19 11:52:15 +0700

    Update chiphersl.c

--- src/chiphersl.c
+++ src/chiphersl.c
@@ -304,12 +304,12 @@ int shamir_cipher(char* input_file)
 void shamir_generate(unsigned long int* p, unsigned long int* c, unsigned long int* d)
 {
   unsigned long int euclid_res[3];
-  generate_prime_number(1, MAXINT, p);
+  *p = generate_prime_number(1, MAXINT);
   do {
   c[0] = generate_prime_too_number(*p - 1, 1, *p - 1);
   c[1] = generate_prime_too_number(*p - 1, 1, *p - 1);
-  euclid(*p - 1, c[0],&d[0],NULL,NULL);
-  euclid(*p - 1, c[1],&d[1],NULL,NULL);
+  equlid(*p - 1, c[0],&d[0],NULL,NULL);
+  equlid(*p - 1, c[1],&d[1],NULL,NULL);
   } while (d[0] > 0xFFFFFF || d[1] > 0xFFFFFF);
 }
 
